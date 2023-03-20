905cd0ddb25586c96001bca8bece9e854eb35740
CaesarJoshua <hernandm999@gmail.com>
2023-03-20 12:26:46 -0600

    a

---
+++ Colorear_por_Leyenda/Colorear_por_Leyenda_resources/js/ardoraColoreaCFG.js
@@ -0,0 +1,36 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=60; timeIni=60; timeBon=0;
+var successes=0; successesMax=0; attempts=0; attemptsMax=1;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=1
+var typeGame=0;
+var tiTime=true;
+var tiTimeType=2;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Bien"; messageTime=""; messageError="Muy Mal"; messageErrorG="Muy Mal"; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="Q29sb3JlYXJfcG9yX0xleWVuZGE="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var joinPar=[];
+var c=["#FF0000","#FFFF00","#0000FF","#00FF00","#800040","#FB6A00","#80FFFF"];
+var x=["371","141","336","324","113","159","186"];
+var y=["106","119","335","215","190","334","262"];
+var t=["57","11","10","9100","444","3.14159","90"];
+var cM=["#FF0000","#FFFF00","#0000FF","#00FF00","#800040","#FB6A00","#80FFFF"];
+var tM=["34+23","22-11","987654321-987654311","9099+1","123+321"," π","45+45"];
+var indexColor=0;
